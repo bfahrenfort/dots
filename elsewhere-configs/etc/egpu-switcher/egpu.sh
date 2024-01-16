32f25fb26ebb07cd5cdb26950849a175aa6348fa
bfahrenfort <bfahrenfort1@gmail.com>
2024-01-16 16:10:51 -0600

    Add font scaling

--- elsewhere-configs/etc/egpu-switcher/egpu.sh
+++ elsewhere-configs/etc/egpu-switcher/egpu.sh
@@ -1,1 +1,10 @@
+function runasuser() {
+    _display_id=":$(find /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+    _username=$(who | grep "\(${_display_id}\)" | awk '{print $1} | head -n 1')
+    _user_id=$(id -u "$_username")
+    _environment=("DISPLAY=$_display_id" "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$_user_id/bus")
+    sudo -Hu "$_username" env "${_environment[@]}" "$@"
+}
+
 cp /etc/egpu-switcher/egpu.conf /etc/X11/xorg.conf.d/99-egpu-switcher.conf
+runasuser gsettings set org.gnome.desktop.interface.text-scaling-factor 1.0
